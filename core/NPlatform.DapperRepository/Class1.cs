097654749e4f3bf6ca974c70996aa41d808913ff
yidl <yidongliang123@163.com>
2021-09-08 17:29:25 +0800

    项目结构调整

--- core/NPlatform.DapperRepository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NPlatform.DapperRepository
-{
-    public class Class1
-    {
-    }
-}
--- core/NPlatform/Result/ITreeResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace NPlatform.Result
-{ 
-    /// <summary>
-    /// 树Result
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public interface ITreeResult<T> : IEPResult 
-    { }
-}
