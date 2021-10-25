b204923a2994ff9beaeb21941d378dfc09b6d020
yidl <yidongliang123@163.com>
2021-10-25 17:08:41 +0800

    EF

---
+++ service/NPlatform.EFRepository/Models/EFContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+using PocManage.Models.TestResource;
+
+#nullable disable
+
+namespace PocManage.Repositories
+{
+    public partial class EFContext : DbContext
+    {
+        public EFContext(DbContextOptions<EFContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<EgineerEvaluate> EgineerEvaluates { get; set; }
+        public virtual DbSet<POCTask> POCTasks { get; set; }
+        public virtual DbSet<POCTaskResource> POCTaskResources { get; set; }
+        public virtual DbSet<TestQuestions> Questions { get; set; }
+        public virtual DbSet<TestResource> Resources { get; set; }
+
+        /// <summary>
+        /// 获取 数据类型
+        /// </summary>
+        /// <typeparam name="T">数据类型</typeparam>
+        /// <returns></returns>
+        public virtual DbSet<T>  GetTDB<T>() where T:class
+        {
+            return this.Set<T>();
+        }
+    }
+}
---
+++ service/NPlatform.EFRepository/Models/IRepository.cs
@@ -0,0 +1,34 @@
+﻿/***********************************************************
+**项目名称:
+**功能描述: 仓储  的摘要说明
+**作    者:   易栋梁
+**版 本 号:    1.0
+**创建日期： 2015/12/7 16:06:56
+**修改历史：
+************************************************************/
+
+using PocManage.Models.TestResource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PocManage.Repositories
+{
+    public interface IRepository<TEntity, TPrimaryKey> where TEntity : BaseModel
+    {
+        Task<TEntity> AddAsync(TEntity item);
+        Task<int> AddOrUpdate(TEntity item);
+        Task<int> AddsAsync(IEnumerable<TEntity> items);
+        Task<bool> Exists(TPrimaryKey key);
+        Task<TEntity> FindByAsync(long key);
+        Task<bool> Remove(long key);
+        Task<bool> Removes(params TEntity[] entitys);
+        IQueryable<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate=null);
+
+        TEntity GetFirstOrDefault(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> GetList(Expression<Func<TEntity, bool>> predicate, Func<TEntity, long> orderBy);
+        (IEnumerable<TEntity>,long total) GetPage(Expression<Func<TEntity, bool>> predicate, Func<TEntity, long> orderBy,int skip,int take);
+    }
+}
